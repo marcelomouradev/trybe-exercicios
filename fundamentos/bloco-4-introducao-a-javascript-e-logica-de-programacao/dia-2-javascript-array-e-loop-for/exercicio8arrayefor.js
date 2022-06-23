aca9ff5486fb84ff4f765214e8cf190a33f5a78e
Marcelo <marceloinblack@gmail.com>
2022-06-22 22:29:53 -0300

    Exercicio 9 resolvido dentro do exercicio 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8arrayefor.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8arrayefor.js
@@ -4,3 +4,7 @@ for (let index = 1; index <= 25; index += 1) {
   numeros.push(index);
 }
 console.log(numeros);
+
+for (let index = 0; index < numeros.length; index += 1) {
+    console.log(numeros[index] / 2);
+}
